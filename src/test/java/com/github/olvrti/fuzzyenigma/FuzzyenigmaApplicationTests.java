469b1c479884b947988706b8297794f0e6571796
Tiago Oliveira <olvrti@gmail.com>
2021-11-15 21:39:19 -0300

    chore: add spring web 2.5

---
+++ src/test/java/com/github/olvrti/fuzzyenigma/FuzzyenigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.olvrti.fuzzyenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzyenigmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
